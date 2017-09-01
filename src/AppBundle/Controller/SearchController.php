c1a7859f9982668e4243f9c6a6a7210edb724585
Ornythorink <orny2007@gmail.com>
2017-09-01 03:25:57 +0200

    ebauche du controleur de  recherches et son template

---
+++ src/AppBundle/Controller/SearchController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Pagerfanta\Adapter\ArrayAdapter;
+use Pagerfanta\Pagerfanta;
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Utils\Configuration;
+
+
+class SearchController extends Controller
+{
+    /**
+     * @Route("/search/", name="search", defaults={"page" = 1})
+     */
+    public function indexAction(Request $request, $page)
+    {
+        $decoded = array();
+        $client = new Client();
+
+        // @todo ne pas oublier le forçage de la locale et gérer les traductions en
+        $locale = $request->attributes->get('_locale');
+        $request->setLocale('fr');
+
+//        $json = $client->request('GET',
+//            Configuration::getApiUrl( $this->container->get('kernel')->getEnvironment() )
+//            . $locale .'/category/sdc/');
+
+        // try / ou null
+       // $decoded = json_decode($json->getBody());
+        $decoded =array();
+        $adapter = new ArrayAdapter($decoded);
+        $pagerfanta = new Pagerfanta($adapter);
+
+        $pagerfanta->setMaxPerPage(20); // 10 by default
+        $maxPerPage = $pagerfanta->getMaxPerPage();
+
+        try  {
+            $pagerfanta->setCurrentPage($page);
+        }catch(NotValidCurrentPageException $e) {
+            throw new NotFoundHttpException('Illegal page');
+        }
+
+        $currentPage = $pagerfanta->getCurrentPage();
+
+        $nbResults = $pagerfanta->getNbResults();
+        $currentPageResults = $pagerfanta->getCurrentPageResults();
+
+        $pagerfanta->getNbPages();
+
+        $pagerfanta->haveToPaginate(); // whether the number of results if higher than the max per page
+
+        return $this->render('AppBundle:Default:search.html.twig' ,
+            array(
+                'locale'      => $locale,
+                'items'       => array(),    /* @todo pourquoi faire  un item  */
+                'brandFilter' => array(),
+                'priceFilter' => array(),
+                'pagination' => $pagerfanta,
+            )
+        );
+    }
+    /**
+     * @Route("/l/{id}", name="linkoffer" ,   options = { "expose" = true }),
+     *
+     */
+    public function countLinkedAction(Request $request, $id)
+    {
+        $locale = $request->attributes->get('_locale');
+        $request->setLocale('fr');
+        $client = new Client();
+
+        $json = $client->request('GET',
+            Configuration::getApiUrl( $this->container->get('kernel')->getEnvironment() )
+            . $locale .'/linked/'. $id);
+        $decoded = json_decode($json->getBody()->getContents() );
+
+        //var_dump($decoded);
+
+        $count = count($decoded[0]->offers);
+        return new JsonResponse($count);
+    }
+
+}
